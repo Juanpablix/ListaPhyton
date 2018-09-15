b4ffc416dc035931a6b7046d9afd24151efc9888
Juanpablix <juanpablogaona@hotmail.com>
2018-09-14 23:19:39 -0500

    jaja k onditrix bandita, esta es una lista de python

---
+++ ListaPython.py
@@ -0,0 +1,10 @@
+Animal = ['Hamster', 'Perro', 'Conejo']
+
+
+Animales_salvajes = ['Tigre', 'Komodo']
+
+##Agregando Animales_salvajes a la lista animal
+Animal.append(Animales_salvajes)
+
+
+print('Updated animal list: ', Animal)
